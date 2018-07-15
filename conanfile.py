dae17562c330ba4cf85b3091d16e3bb008e43961
Karl Wallner <kwallner@mail.de>
2018-07-15 15:38:58 +0200

    Added Install. Initial conan support.

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+import os
+from conans import ConanFile, CMake
+
+class libiconvConan(ConanFile):
+    name = "libiconv"
+    version = "1.15.0"
+    branch = "master"
+    generators = "cmake"
+    settings =  "os", "compiler", "arch", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=True"
+    url = "http://github.com/kwallner/libiconv"
+    scm = { "type": "git", "url": "auto", "revision": "auto" }
+    
+    def config_options(self):
+        del self.settings.compiler.libcxx
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.install()
+
+    def package_info(self):
+        self.env_info.libiconv_DIR = os.path.join(self.package_folder, 'setup')
+   
